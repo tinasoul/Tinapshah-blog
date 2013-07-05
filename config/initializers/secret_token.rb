fb4a8b708d3c02f09ce7c94c065f43fa418c8c26
tinasoul <tinapshah@gmail.com>
2013-07-05 14:29:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinapshahBlog::Application.config.secret_token = 'ec4038f5b97887d200f48cd8c5a98571626b48679fa374cb2d09699dad90d583d62b6d4d36692202b635fd93c36b1b312b7c91eb57fdaa86285ab9ac86e59ab4'
