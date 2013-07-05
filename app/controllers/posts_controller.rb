ed14529141709db03eb39ea74e1bb9ed9a9ac283
tinasoul <tinapshah@gmail.com>
2013-07-05 15:53:22 -0500

    Generated rails model and created blog post scaffold with title and body. Index page shows title and body of 5 posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
 
 	def index
-	
+		@name = "Tina"
+		# @post_titles = ["Why code", "What I learned in 3 weeks"]
+		# lesson 17 of http://learn.themakersquare.com/pages/rails/practicing-building-a-new-rails-app
+		@posts = Post.all
 	end
 
 
